7e19db5bb8120068cfa8c6ccc7498eb8a62d9a37
Oleh Kozina <oleh@gearboxbuilt.com>
2025-10-01 20:47:06 -0700

    fixes footer images

--- app/components/Footer/FooterContainer.tsx
+++ app/components/Footer/FooterContainer.tsx
@@ -9,7 +9,7 @@ const FooterContainer = ({
   footerContainer?: NavigationType;
 }) => {
   return (
-    <div className="container">
+    <div className="container z-content relative">
       <div className="flex flex-col md:flex-row md:gap-10">
         <nav className="flex text-center flex-grow flex-col md:flex-row">
           <div className=" mt-4 flex justify-center md:inline-block md:justify-start md:mt-0 z-[5]">
--- app/components/Footer/FooterImages.tsx
+++ app/components/Footer/FooterImages.tsx
@@ -13,14 +13,14 @@ const FooterImages = ({
         alt="bread-left"
         width={100}
         height={24}
-        className="w-[11rem] h-[8rem] bg-no-repeat absolute bottom-10 left-0 z-[-1] lg:z-0"
+        className="w-[11rem] bg-no-repeat absolute max-md:top-0 md:bottom-0 left-0"
       />
       <Image
         src={footerImages?.right || "/bread-large.png"}
         alt="bread-left"
         width={80}
         height={24}
-        className="absolute bottom-0 right-0 w-[20rem] bg-no-repeat md:w-[16rem]"
+        className="absolute bottom-0 right-0 w-[8rem] bg-no-repeat md:w-[10rem]"
       />
     </>
   );
